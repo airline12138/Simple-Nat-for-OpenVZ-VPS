e49b67e6659dc4f828f35f0ca213613f1209ef07
airline12138 <70080598+airline12138@users.noreply.github.com>
2020-10-02 15:10:44 +0800

    Update addnat.sh

--- addnat.sh
+++ addnat.sh
@@ -6,7 +6,7 @@ echo -e "Please enter how many / 24 IP addresses are needed, max: 5."
 		stty erase '^H' && read -p "(such as 1):" user_ip_num
 		[[ -z "${user_ip_num}" ]] && echo -e "cancel..." && exit 1
 iptables -t nat -F
-iptables -t nat -A POSTROUTING -o eth1 -j SNAT --to ${main_ip}
+iptables -t nat -A POSTROUTING -o eth0 -j SNAT --to ${main_ip}
 user_ip_head="10.0."
 for (( c = 1; c <= ${user_ip_num}; c++ ));do
 	for (( d = 1; d <= 255; d++ ));do
@@ -24,13 +24,11 @@ for (( c = 1; c <= ${user_ip_num}; c++ ));do
 			user_port_first="1"${d}"0"
 			user_port_last="1"${d}"9"
 		fi
-		iptables -t nat -A PREROUTING -i eth1 -p tcp -m tcp --dport ${ssh_port} -j DNAT --to-destination ${user_ip}:22
-		iptables -t nat -A PREROUTING -i eth1 -p tcp -m tcp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}
-		iptables -t nat -A PREROUTING -i eth1 -p udp -m udp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}	
+		iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport ${ssh_port} -j DNAT --to-destination ${user_ip}:22
+		iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}
+		iptables -t nat -A PREROUTING -i eth0 -p udp -m udp --dport ${user_port_first}:${user_port_last} -j DNAT --to-destination ${user_ip}	
 	done
 done
 service iptables save
 service iptables restart
-echo -e "Latest version: https://github.com/qq2312890354/addnat.sh/"
-echo -e "Nothing to do!"
 
